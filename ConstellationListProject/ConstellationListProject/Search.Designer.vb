7720e6467c6d4ef64454a13e1dce7512dd26bc76
kaz-saito <k-saito@plus-bm.jp>
2017-07-18 14:31:55 +0900

    初期投入
    kaz-saito

---
+++ ConstellationListProject/ConstellationListProject/Search.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Search
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.condition = New System.Windows.Forms.ComboBox()
+        Me.searchButton = New System.Windows.Forms.Button()
+        Me.resultView = New System.Windows.Forms.ListView()
+        Me.nextButton = New System.Windows.Forms.Button()
+        Me.backButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("メイリオ", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(94, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(172, 55)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "星座検索"
+        '
+        'condition
+        '
+        Me.condition.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.condition.FormattingEnabled = True
+        Me.condition.Location = New System.Drawing.Point(85, 108)
+        Me.condition.MaxDropDownItems = 4
+        Me.condition.Name = "condition"
+        Me.condition.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.condition.Size = New System.Drawing.Size(182, 20)
+        Me.condition.TabIndex = 1
+        '
+        'searchButton
+        '
+        Me.searchButton.Location = New System.Drawing.Point(274, 101)
+        Me.searchButton.Name = "searchButton"
+        Me.searchButton.Size = New System.Drawing.Size(54, 32)
+        Me.searchButton.TabIndex = 2
+        Me.searchButton.Text = "検索"
+        Me.searchButton.UseVisualStyleBackColor = True
+        '
+        'resultView
+        '
+        Me.resultView.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar
+        Me.resultView.Font = New System.Drawing.Font("メイリオ", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.resultView.ForeColor = System.Drawing.SystemColors.MenuText
+        Me.resultView.Location = New System.Drawing.Point(86, 145)
+        Me.resultView.Name = "resultView"
+        Me.resultView.Size = New System.Drawing.Size(140, 175)
+        Me.resultView.TabIndex = 5
+        Me.resultView.UseCompatibleStateImageBehavior = False
+        Me.resultView.View = System.Windows.Forms.View.List
+        '
+        'nextButton
+        '
+        Me.nextButton.Location = New System.Drawing.Point(223, 335)
+        Me.nextButton.Name = "nextButton"
+        Me.nextButton.Size = New System.Drawing.Size(75, 23)
+        Me.nextButton.TabIndex = 6
+        Me.nextButton.Text = ">>"
+        Me.nextButton.UseVisualStyleBackColor = True
+        '
+        'backButton
+        '
+        Me.backButton.Location = New System.Drawing.Point(12, 335)
+        Me.backButton.Name = "backButton"
+        Me.backButton.Size = New System.Drawing.Size(75, 23)
+        Me.backButton.TabIndex = 7
+        Me.backButton.Text = "<<"
+        Me.backButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(154, 340)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 12)
+        Me.Label2.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(329, 335)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(57, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "戻る"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Search
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 392)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.backButton)
+        Me.Controls.Add(Me.nextButton)
+        Me.Controls.Add(Me.resultView)
+        Me.Controls.Add(Me.searchButton)
+        Me.Controls.Add(Me.condition)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Search"
+        Me.Text = "Search"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents condition As ComboBox
+    Friend WithEvents searchButton As Button
+    Friend WithEvents resultView As ListView
+    Friend WithEvents nextButton As Button
+    Friend WithEvents backButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ ConstellationListProject/ConstellationListProject/Search.vb
@@ -0,0 +1,195 @@
+﻿Public Class Search
+
+    Private constellationList As List(Of String)
+    Private page As Integer = 0
+    Private startPage As Integer = 0
+    Private countPage As Integer
+    Private maxPage As Integer
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles searchButton.Click
+
+        Dim season As String = condition.SelectedValue
+        Dim dao As New SearchDao
+
+        constellationList = dao.searchConstellation(season)
+
+        resultView.Clear()
+
+        resultView.View = View.Details
+
+        resultView.Columns.Add("星座名", 135, HorizontalAlignment.Center)
+
+        For i As Integer = 0 To 4
+
+            If i >= 0 And i < constellationList.Count() Then
+                resultView.Items.Add(constellationList(i))
+
+                If i Mod 2 = 0 Then
+                    resultView.Items(i).BackColor = Color.Cyan
+                End If
+
+            End If
+
+        Next
+
+        page = 0
+
+        maxPage = Math.Ceiling(constellationList.Count() / 5)
+
+        countPage = 1
+
+        Label2.Text = countPage & "/" & maxPage
+
+        resultView.GridLines = True
+
+
+    End Sub
+
+    Private Sub Search_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        'DataTableオブジェクトの作成
+        Dim seasonTable As New DataTable()
+
+        'DataTableに列追加
+        seasonTable.Columns.Add("ID", GetType(String))
+        seasonTable.Columns.Add("NAME", GetType(String))
+
+        '配列の用意
+        Dim rowDataArray As String(,) = {{"0", "春"},
+                                        {"1", "夏"},
+                                        {"2", "秋"},
+                                        {"3", "冬"}}
+
+        For i As Integer = 0 To rowDataArray.GetLength(0) - 1
+            '新しい行を作成
+            Dim row As DataRow = seasonTable.NewRow()
+
+            '各列に値をセット
+            row("ID") = rowDataArray(i, 0)
+            row("NAME") = rowDataArray(i, 1)
+
+            'DataTableに行を追加
+            seasonTable.Rows.Add(row)
+        Next
+
+        'コンボボックスのDataSourceにDataTableを割り当てる
+        condition.DataSource = seasonTable
+
+        '表示される値はDataTableのNAME列
+        condition.DisplayMember = "NAME"
+
+        '対応する値はDataTableのID列
+        condition.ValueMember = "ID"
+
+    End Sub
+
+    Private Sub resultView_ColumnWidthChanging(sender As Object, e As ColumnWidthChangingEventArgs) Handles resultView.ColumnWidthChanging
+        e.NewWidth = Me.resultView.Columns(e.ColumnIndex).Width
+        e.Cancel = True
+    End Sub
+
+    Private Sub nextButton_Click(sender As Object, e As EventArgs) Handles nextButton.Click
+
+
+        If page + 5 >= 0 And page + 5 < constellationList.Count() Then
+
+            startPage = page + 5
+
+            resultView.Clear()
+
+            resultView.View = View.Details
+
+            resultView.Columns.Add("星座名", 135, HorizontalAlignment.Center)
+
+            Dim i As Integer = 0
+
+            For page = page + 5 To page + 9
+
+                If constellationList.Count() > page Then
+                    resultView.Items.Add(constellationList(page))
+
+                    i = i + 1
+
+                Else
+
+                    Exit For
+
+                End If
+
+
+            Next
+
+            For j As Integer = 0 To i
+                If j Mod 2 = 0 Then
+                    resultView.Items(j).BackColor = Color.Cyan
+                End If
+            Next
+
+            page = startPage
+
+            countPage = countPage + 1
+
+            Label2.Text = countPage & "/" & maxPage
+
+            resultView.GridLines = True
+
+        End If
+
+    End Sub
+
+    Private Sub backButton_Click(sender As Object, e As EventArgs) Handles backButton.Click
+
+        If page - 5 >= 0 And page - 5 < constellationList.Count() Then
+
+            startPage = page - 5
+
+            resultView.Clear()
+
+            resultView.View = View.Details
+
+            resultView.Columns.Add("星座名", 135, HorizontalAlignment.Center)
+
+            Dim i As Integer = 0
+
+            For page = page - 5 To page - 1
+
+                If constellationList.Count() > page Then
+                    resultView.Items.Add(constellationList(page))
+
+                    i = i + 1
+
+                Else
+
+                    Exit For
+
+                End If
+
+            Next
+
+            For j As Integer = 0 To i
+                If j Mod 2 = 0 Then
+                    resultView.Items(j).BackColor = Color.Cyan
+                End If
+            Next
+
+            page = startPage
+
+            countPage = countPage - 1
+
+            Label2.Text = countPage & "/" & maxPage
+
+            resultView.GridLines = True
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim f As New MainMenu
+        f.Show()
+        My.Application.ApplicationContext.MainForm = f
+        Me.Close()
+
+    End Sub
+End Class
