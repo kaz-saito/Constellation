15694cbd06000a3c92460025c10c0a53f1770583
t-ohta1 <t-ohta@plus-bm.jp>
2017-07-18 17:28:43 +0900

    ユーザーメンテ追加機能作成
    t-ohta1

--- ConstellationListProject/ConstellationListProject/DataAdder.Designer.vb
+++ ConstellationListProject/ConstellationListProject/DataAdder.Designer.vb
@@ -25,6 +25,13 @@ Partial Class DataAdder
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -54,11 +61,75 @@ Partial Class DataAdder
         Me.Button1.Text = "戻る"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(81, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 18)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "氏名"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(119, 113)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(127, 25)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(45, 167)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(68, 18)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "パスワード"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(119, 164)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(127, 25)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(69, 201)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 18)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "再入力"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(119, 198)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(127, 25)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(131, 257)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "追加"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'DataAdder
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(332, 346)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -74,4 +145,11 @@ Partial Class DataAdder
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
 End Class
--- ConstellationListProject/ConstellationListProject/DataAdder.vb
+++ ConstellationListProject/ConstellationListProject/DataAdder.vb
@@ -2,7 +2,11 @@
 
     Dim maintenanceType As Integer
     Dim endType As Integer
+    Dim cn As System.Data.SqlClient.SqlConnection
     Private Sub DataAdder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        TextBox2.PasswordChar = "*"
+        TextBox3.PasswordChar = "*"
 
         If Label2.Text = "ユーザー" Then
 
@@ -43,6 +47,169 @@
 
             My.Application.ApplicationContext.MainForm = MasterMaintenance
 
+        ElseIf endType = 2 Then
+
+            DoneMaintenance.Show()
+
+            My.Application.ApplicationContext.MainForm = DoneMaintenance
+
         End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Try
+
+            If lengthCheck() And matchPassCheck() Then
+
+                connectDb()
+
+                insertProcess(getNextUserId())
+
+                endType = 2
+
+                Me.Close()
+
+            End If
+
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message())
+
+        End Try
+    End Sub
+
+    Function lengthCheck() As Boolean
+
+        Dim flag As Boolean = True
+
+        If TextBox1.Text.Length > 45 Or TextBox1.Text.Length = 0 Then
+
+            MsgBox("氏名は全角15文字以内で入力してください")
+            flag = False
+
+        End If
+
+        If TextBox2.Text.Length > 16 Or TextBox2.Text.Length = 0 Then
+
+            MsgBox("パスワードは半角16文字以内で入力してください")
+            flag = False
+
+        End If
+
+        Return flag
+
+    End Function
+
+    Function matchPassCheck() As Boolean
+
+        If TextBox2.Text = TextBox3.Text Then
+
+            Return True
+
+        Else
+
+            MsgBox("パスワードが一致しません")
+
+            Return False
+
+        End If
+
+    End Function
+
+    Sub connectDb()
+
+        Try
+
+            Dim serverName As String = "192.168.0.173"
+            Dim dbName As String = "master"
+            Dim userName As String = "sa"
+            Dim password As String = "SaPassword2017"
+
+            cn = New System.Data.SqlClient.SqlConnection()
+
+            cn.ConnectionString =
+                "Data Source = " & serverName &
+                ";Initial Catalog = " & dbName &
+                ";User ID = " & userName &
+                ";Password = " & password
+
+            cn.Open()
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Function getNextUserId() As String
+
+        Try
+
+            Dim cmd As New System.Data.SqlClient.SqlCommand
+            Dim sdr As SqlClient.SqlDataReader
+
+            Dim sqlStr As String
+            Dim nextId As String
+
+            cmd.Connection = cn
+            cmd.CommandType = CommandType.Text
+
+            sqlStr = "SELECT MAX(USER_ID) + 1 FROM M_USER"
+
+            cmd.CommandText = sqlStr
+
+            sdr = cmd.ExecuteReader()
+
+            cmd.Dispose()
+
+            sdr.Read()
+
+            nextId = sdr(0).ToString()
+
+            sdr.Close()
+
+            If nextId.Length = 1 Then
+
+                Return 0 & nextId
+
+            Else
+
+                Return nextId
+
+            End If
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message())
+            Throw New Exception
+
+        End Try
+
+    End Function
+
+    Sub insertProcess(ByVal id As String)
+
+        Dim cmd As New System.Data.SqlClient.SqlCommand
+        Dim sqlStr As String
+
+        cmd.Connection = cn
+
+        cmd.CommandType = CommandType.Text
+
+        sqlStr = "INSERT INTO M_USER(USER_ID, USER_NAME, PASSWORD, DELETE_FLAG,
+                    CREATE_DATE, CREATE_USER)"
+        sqlStr += "VALUES('" & id & "','" & TextBox1.Text & "','" & TextBox2.Text
+        sqlStr += "', 0, GETDATE(), '00')"
+
+        cmd.CommandText = sqlStr
+
+        cmd.ExecuteNonQuery()
+
+        cmd.Dispose()
+
     End Sub
 End Class
